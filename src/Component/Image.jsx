0771bbd5cd6f8f33822997e6a454c24648686607
YangTzuHung <j25889651556@gmail.com>
2022-02-18 19:12:40 +0800

    202202181912

---
+++ src/Component/Image.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Image extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+ 
+export default Image;
+<div>
+    
+</div>
