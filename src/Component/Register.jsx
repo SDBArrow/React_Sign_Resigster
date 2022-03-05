8f7648451df63c47a0d2a19f2c3d6c5693107ae9
YangTzuHung <j25889651556@gmail.com>
2022-03-06 04:46:34 +0800

    202203060446

--- src/Component/Register.jsx
+++ src/Component/Register.jsx
@@ -2,7 +2,11 @@ import React, { Component} from 'react';
 import { Link } from "react-router-dom";
 
 class Register extends Component{
-    state = {  } 
+    state = { 
+        email: null,
+        password: null,
+        passwordcheck: null
+     } 
 
     registeraccount(){
         
