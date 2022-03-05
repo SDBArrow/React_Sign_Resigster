772fcf312fff2089c6b741d4c3fc514b3ffd1e65
YangTzuHung <j25889651556@gmail.com>
2022-03-06 04:35:15 +0800

    202203061635

--- src/Component/Image.jsx
+++ src/Component/Image.jsx
@@ -5,11 +5,10 @@ class Image extends Component {
     state = {  } 
     render() { 
         return (
-            <div class ="ml-0 sm:ml-24">
-                <p class="text-center text-3xl text-blue-400 sm:mt-24 ">AGV WebAPP</p>
-                <img class = "rounded-lg sm:h-72 mt-10 sm:mt-24 w-96 shadow-2xl" src={logo2} alt="Image"/>
+            <div className ="ml-0 sm:ml-24">
+                <p className="text-center text-3xl text-blue-400 sm:mt-24 ">AGV WebAPP</p>
+                <img className = "rounded-lg sm:h-72 mt-10 sm:mt-24 w-96 shadow-2xl" src={logo2} aria-hidden alt="Image"/>
             </div>
-
         );
     }
 }
